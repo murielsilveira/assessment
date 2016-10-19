61f61a38d7a7bca1debf80fe7fb17e47169a01e6
Muriel Silveira <murielkong@gmail.com>
2016-10-18 23:02:39 -0200

    Add a first test to guarantee that we'll only parse numbers

--- number-to-phrase.js
+++ number-to-phrase.js
@@ -1,3 +1,11 @@
-module.exports = function(number) {
+function numberToPhrase(numberToParse) {
+  if (!isNumber(numberToParse))
+    return 'Unknown number'
   return 'one'
 }
+
+function isNumber(number) {
+  return typeof(number) === 'number'
+}
+
+module.exports = numberToPhrase
--- number-to-phrase.test.js
+++ number-to-phrase.test.js
@@ -2,6 +2,10 @@ const expect = require('chai').expect
 const numberToPhrase = require('./number-to-phrase')
 
 describe('number to phrase tests', function(){
+  it('should parse only numbers', function(){
+    expect(numberToPhrase('A')).to.equal('Unknown number')
+  })
+
   it('1 should be: one', function(){
     expect(numberToPhrase(1)).to.equal('one')
   })
