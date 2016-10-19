fbfc7461a18c495b8e64a575bdf4a536b10e0d6b
Muriel Silveira <murielkong@gmail.com>
2016-10-18 23:16:51 -0200

    Simplify validation

--- number-to-phrase.js
+++ number-to-phrase.js
@@ -1,11 +1,7 @@
 function numberToPhrase(numberToParse) {
-  if (!isNumber(numberToParse))
+  if (typeof(numberToParse) !== 'number')
     return 'Unknown number'
   return 'one'
-}
-
-function isNumber(number) {
-  return typeof(number) === 'number'
 }
 
 module.exports = numberToPhrase
