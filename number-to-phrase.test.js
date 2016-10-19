746b5b54b2afedd0f9972bbc67915982e11bd889
Muriel Silveira <murielkong@gmail.com>
2016-10-18 23:10:41 -0200

    Add more assertions to ensure that we only parse numbers

--- number-to-phrase.test.js
+++ number-to-phrase.test.js
@@ -2,8 +2,18 @@ const expect = require('chai').expect
 const numberToPhrase = require('./number-to-phrase')
 
 describe('number to phrase tests', function(){
-  it('should parse only numbers', function(){
+  it('should not parse non numbers', function(){
     expect(numberToPhrase('A')).to.equal('Unknown number')
+    expect(numberToPhrase(',')).to.equal('Unknown number')
+    expect(numberToPhrase('√')).to.equal('Unknown number')
+    expect(numberToPhrase('')).to.equal('Unknown number')
+    expect(numberToPhrase({})).to.equal('Unknown number')
+    expect(numberToPhrase([])).to.equal('Unknown number')
+    expect(numberToPhrase(function(){})).to.equal('Unknown number')
+    expect(numberToPhrase(undefined)).to.equal('Unknown number')
+    expect(numberToPhrase(null)).to.equal('Unknown number')
+    expect(numberToPhrase(false)).to.equal('Unknown number')
+    expect(numberToPhrase(true)).to.equal('Unknown number')
   })
 
   it('1 should be: one', function(){
